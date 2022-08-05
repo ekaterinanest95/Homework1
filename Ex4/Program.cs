53c99cb2db84724813f01f31443a16574322e426
Ekaterina <katya.kinal@gmail.com>
2022-08-05 12:54:52 +0300

    Задание 4 Задача 4

---
+++ Ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if(numberB > max)
+{
+    max = numberB;
+}
+else
+{
+    max = numberA;
+}
+if(numberC > max)
+{
+    max = numberC;
+    Console.Write(max);
+}
+else
+{
+    Console.Write(max);
+}
