871782ca1a8ca086d8a34d4e6f03f99ad3307648
Ekaterina <katya.kinal@gmail.com>
2022-08-05 12:39:50 +0300

    Задача 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine("max = " + numberA);
+}
+else
+{
+    Console.WriteLine("max = " + numberB);
+}
