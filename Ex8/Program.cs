af950659566de60695bfa317189f18bfc6f372c7
Ekaterina <katya.kinal@gmail.com>
2022-08-05 13:22:54 +0300

    Задача 8

---
+++ Ex8/Program.cs
@@ -0,0 +1,18 @@
+﻿    Console.Write("Введите чило N: ");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+    int count = 1;
+
+    while (count <= numberN)
+    {
+        if(count %2 == 0)
+        {
+            Console.WriteLine(count);
+            count++;
+        }
+        else
+        {
+            count++;
+        }    
+    }
+
+
